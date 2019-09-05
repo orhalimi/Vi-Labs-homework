0abb9249fcce8cd5f8008d64df77b79ccbc9359f
Or Halimi <or50002001@gmail.com>
2019-09-05 12:01:03 +0300

    streams and fenchy cache

--- endpoints/wordscount.endpoint.js
+++ endpoints/wordscount.endpoint.js
@@ -1,7 +1,7 @@
 import wordCountModule from '../modules/wordcount.module';
 
 async function getWordsCount(req, res) {
-    let result = await wordCountModule.getWordsCount('TheKingJamesBible.txt');
+    let result = await wordCountModule.getWordsCount('http://www.gutenberg.org/cache/epub/10/pg10.txt');
     res.json(result);
 }
 
--- modules/wordcount.module.js
+++ modules/wordcount.module.js
@@ -1,32 +1,56 @@
 import fs from 'fs';
 import path from 'path';
-const {promisify} = require('util');
+import moment from 'moment';
+import readline from 'readline';
+const {promisify} = require('util'); 
+const request = require('request');
 
 const readFileAsync = promisify(fs.readFile);
-const resources = {};
-const resourcesPath = path.join(__dirname, '../resources/');
+const writeFileAsync = promisify(fs.writeFile);
 
-async function getWordsCount(filename){
-    const countResults = {};
-    if(!resources[filename]) {
-        try{
-            resources[filename] = await readFileAsync(path.join(resourcesPath, filename), 'utf8');
-        } catch(e){
-            return {error: `couldn't load file at path ${path.join(resourcesPath, filename)}`};
-        }
+const dataPath = path.join(__dirname, '../data/');
+const totalResults = {};
+
+async function getWordsCount(url){
+    if(!url) return { error: 'invalid url'}
+    // if we have updated cache response return it
+    if(totalResults[url] && totalResults[url]['expiry'] && moment().isBefore(totalResults[url]['expiry']) &&
+     fs.existsSync(totalResults[url]['path'])) {
+        const fileData = await readFileAsync(totalResults[url]['path'], 'utf8');
+        return JSON.parse(fileData);
     }
-    const file = resources[filename];
-    const mappedResource = file? file.split(/\s+/) : [];
-    mappedResource.forEach(word => {
-        const alphaChars = word.match(/[a-z]/gi, '');
-        if (!alphaChars) return;
-        let alphaWord = alphaChars.join('');
-        if (!countResults[alphaWord]) countResults[alphaWord]= {count:0, word: alphaWord};
-        countResults[alphaWord].count +=1;
+
+    const countResults = {};
+    const readlinePromise = new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: request.get(url).on('error', (err) => reject(err)) 
+        });
+        rl.on('line', (line) => {
+            const mappedResource = line? line.split(/\s+/) : [];
+            mappedResource.forEach(word => { 
+                const alphaChars = word.match(/[a-z]/gi, '');
+                if (!alphaChars) return;
+                let alphaWord = alphaChars.join('');
+                if (!countResults[alphaWord]) countResults[alphaWord]= {count:0, word: alphaWord};
+                countResults[alphaWord].count +=1;
+            });
+        })
+        .on('close', () => resolve());
     });
+
+    try { 
+        await readlinePromise;
+    } catch(e) {
+        return { error: `Couldn't parse data. Error: ${e.message}`};
+    }
+
     Object.keys(countResults).forEach(key => {
         countResults[key].isPrime = isPrime(countResults[key].count);
     });
+
+    const savedResultPath = path.join(dataPath + Buffer.from(url).toString('base64')) + '.json';
+    await writeFileAsync(savedResultPath, JSON.stringify(countResults));
+    totalResults[url] = {expiry: moment().add(3, 'hours'), path: savedResultPath};
     return countResults;
 }
 
